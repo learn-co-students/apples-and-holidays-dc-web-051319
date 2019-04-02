c8f95fcefa95c211fafad3d0681a891500e61bd7
Matt Williams <matt.seongjun@gmail.com>
2019-04-01 20:02:13 -0400

    Uncomment test for printing a whole string at the end of #all_supplies_in_holidays

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,15 @@ end
 def all_holidays_with_bbq(holiday_hash)
     # return an array of holiday names (as symbols) where supply lists
     # include the string "BBQ"
-
+    bbq_days = []
+    holiday_hash.each do |season, holidays|
+        holidays.each do |holiday, supplies|
+            if supplies.include?("BBQ") 
+                bbq_days << holiday
+            end
+        end
+    end
+    bbq_days
 end
 
 holiday_hash = {
